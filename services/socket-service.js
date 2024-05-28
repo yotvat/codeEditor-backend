d36dcc22d0d3b0d1e0b2de8f66107769b61dbdb2
yotvat <idooy11@gmail.com>
2024-05-28 09:26:55 +0300

    Fix socket bug (first entrance)

--- services/socket-service.js
+++ services/socket-service.js
@@ -2,13 +2,15 @@ import { Server } from 'socket.io'
 import { logger } from './logger.service.js'
 
 var gIo = null
+var mentorSocketId = null
 
 export const SOCKET_EVENT_SET_BLOCK = 'set-block'
 export const SOCKET_EMIT_UPDATED_BLOCK = 'block-updated'
 export const SOCKET_EVENT_UPDATE_BLOCK = 'update-block'
 
 export const SOCKET_EMIT_IS_MENTOR = 'is-mentor'
-let onlineUsers = 0
+
+// let onlineUsers = 0
 
 export const socketService = {
   setupSocketAPI,
@@ -23,20 +25,18 @@ export function setupSocketAPI(server) {
 
   gIo.on('connection', socket => {
     logger.info(`New connected socket[id:${socket.id}]`)
-    onlineUsers++
-    logger.info(`Online users: ${onlineUsers}`)
-    const isMentor = onlineUsers === 1
-    socket.emit(SOCKET_EMIT_IS_MENTOR, { isMentor })
+    // onlineUsers++
+    // logger.info(`Online users: ${onlineUsers}`)
 
-    // socket.on('disconnect', socket => {
-    //   onlineUsers--
-    //   logger.info(`socket disconnected [id:${socket.id}]`)
-    // })
-
-    socket.on('disconnect',() => {
-      onlineUsers--
+    socket.on('disconnect', () => {
+      // onlineUsers--
       logger.info(`Socket disconnected [id: ${socket.id}]`);
-      logger.info(`Online users: ${onlineUsers}`)
+      // logger.info(`Online users: ${onlineUsers}`)
+
+      if (socket.id === mentorSocketId) {
+        mentorSocketId = null
+      }
+
     })
 
     socket.on(SOCKET_EVENT_SET_BLOCK, block => {
@@ -49,6 +49,13 @@ export function setupSocketAPI(server) {
       socket.join(block._id)
       logger.info(`joined block: ${block._id}`)
       socket.myBlock = block._id
+
+      //assign mentor
+      const isMentor = mentorSocketId === null
+      if (isMentor) {
+        mentorSocketId = socket.id
+      }
+      socket.emit(SOCKET_EMIT_IS_MENTOR, { isMentor })
 
     })
 
