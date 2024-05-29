2e9440f55e350838f3312e6817303ce2710d8cc9
yotvat <idooy11@gmail.com>
2024-05-29 11:09:20 +0300

    Init build

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,5 @@
 export default {
   dbURL:
-    'mongodb+srv://idoyotvat:yotvat26@cluster0.9yob3ir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  // dbURL: 'mongodb://127.0.0.1:27017',
+  process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
   dbName: 'block_db',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 export default {
   dbURL:
-    process.env.MONGO_URL ||
-    'mongodb+srv://idoyotvat:idoyotvat26@cluster0.9yob3ir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    process.env.MONGO_URL,
   dbName: process.env.DB_NAME || 'block_db',
 }
